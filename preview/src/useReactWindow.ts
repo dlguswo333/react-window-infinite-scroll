c249670c5a049c68bb8082b16e996e6534ce223f
dlguswo333 <dlguswo002@gmail.com>
2023-07-29 19:01:03 +0900

    Edit useReactWindow to test loadMoreItems logics

--- preview/src/useReactWindow.ts
+++ preview/src/useReactWindow.ts
@@ -2,36 +2,40 @@ import {useCallback, useMemo, useRef, useState} from 'react';
 import {sleep} from './util';
 
 const MAX_LENGTH = 500;
-const SLEEP_MS = 10;
+const SLEEP_MS = 100;
 
 const useReactWindow = () => {
   const [data, setData] = useState<string[]>([]);
   const isFetching = useRef<boolean>(false);
   const itemCount = useMemo(() => data.length + (data.length < MAX_LENGTH ? 1 : 0), [data.length]);
 
+  /**
+   * Intentionally depend on `data` state to test
+   * if `loadMoreItems` can add new items without duplicated items.
+   */
   const loadMoreItems = useCallback(async () => {
     if (isFetching.current) {
       return;
     }
     isFetching.current = true;
+    const newValue = `data ${data.length}`;
     await sleep(SLEEP_MS);
     setData(data => {
       if (data.length >= MAX_LENGTH) {
         // This must not be reached.
         throw Error(`data length exceeds MAX_LENGTH:${MAX_LENGTH}`);
       }
-      const newData = `data ${data.length}`;
-      return data.concat([newData]);
+      return data.concat([newValue]);
     });
     isFetching.current = false;
-  }, []);
+  }, [data]);
 
   const isItemsLoaded = useCallback((index: number) => {
     return index >= MAX_LENGTH || index < data.length;
   }, [data.length]);
 
   return {
-    data, loadMoreItems, isItemsLoaded, itemCount
+    data, loadMoreItems, isItemsLoaded, itemCount,
   };
 };
 
