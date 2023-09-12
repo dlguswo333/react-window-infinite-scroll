2bc8c7bd57b43dbe521a80c6237cc0cef60cdd82
dlguswo333 <dlguswo002@gmail.com>
2023-09-12 22:29:59 +0900

    Add simple dynamic data test component

--- package/test/Components.tsx
+++ package/test/Components.tsx
@@ -1,4 +1,4 @@
-import {CSSProperties, useRef} from 'react';
+import {CSSProperties, useRef, useState} from 'react';
 import InfiniteScroll from '../src/InfiniteScroll';
 import {FixedSizeList} from 'react-window';
 import React from 'react';
@@ -49,6 +49,51 @@ export const StaticData2 = () => {
   return <InfiniteScroll
     data={data}
     loadMoreItems={loadMoreItems}
+    isItemLoaded={isItemsLoaded}
+    itemCount={itemCount}
+    threshold={1}
+    outerRef={outerRef}
+    scrollOffset={30}
+  >
+    {({onItemsRendered}) => <FixedSizeList
+      height={300}
+      itemCount={itemCount}
+      width='100%'
+      onItemsRendered={onItemsRendered}
+      itemSize={30}
+      outerRef={outerRef}
+      className='Outer'
+    >
+      {Row}
+    </FixedSizeList>}
+  </InfiniteScroll>;
+};
+
+export const SimpleDynamicData = ({hasInitialData, howToLoad}: {hasInitialData: boolean; howToLoad: 'sync' | 'instantAsync' | 'fastAsync' | 'slowAsync'}) => {
+  const [data, setData] = useState<string[]>(hasInitialData ? ['0'] : []);
+  const outerRef = useRef<HTMLElement>(null);
+  const loadMoreItemsSync = () => {
+    setData(data.concat(data.length.toString()));
+  };
+  const loadMoreItemsAsync = async () => {
+    if (howToLoad !== 'instantAsync') {
+      await new Promise(res => setTimeout(res, howToLoad === 'fastAsync' ? 0 : 500));
+    }
+    setData(data.concat(data.length.toString()));
+  };
+  const isItemsLoaded = (index: number) => {
+    const ret = !(index === data.length && index < 3);
+    return ret;
+  };
+  const itemCount = data.length + (data.length < 3 ? 1 : 0);
+
+  const Row = ({index, style}: {index: number, style: CSSProperties}) => (
+    <div style={style}>{data[index]}</div>
+  );
+
+  return <InfiniteScroll
+    data={data}
+    loadMoreItems={howToLoad === 'sync' ? loadMoreItemsSync : loadMoreItemsAsync}
     isItemLoaded={isItemsLoaded}
     itemCount={itemCount}
     threshold={1}
