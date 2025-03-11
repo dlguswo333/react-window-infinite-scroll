8f7b7e5e3a9a7681138bd744889bbaf67feeb633
dlguswo333 <dlguswo002@gmail.com>
2025-03-11 20:22:58 +0900

    [#46] Add layout to props

--- package/src/InfiniteScroll.tsx
+++ package/src/InfiniteScroll.tsx
@@ -37,6 +37,8 @@ type Props = {
   outerRef: React.RefObject<HTMLElement> | HTMLElement;
   /** *Actual* data that needs to be virtually scrolled. */
   data: unknown[];
+  /** Layout of the vertical list. Default is `'vertical'`. */
+  layout: 'vertical' | 'horizontal';
 }
 
 /** This is arbitrary number thus subject to change. */
